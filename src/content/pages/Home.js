fc80804365efd7aa81428d2bc60bfacdcdf4822c
spazevic <spazevic@yahoo.com>
2020-06-09 11:20:07 -0700

    saved changes

--- src/content/pages/Home.js
+++ src/content/pages/Home.js
@@ -2,19 +2,6 @@ import React, {useEffect, useState} from 'react'
 
 
 const Home = props => {
-	useEffect(() => {
-		getAnimeData()
-	}, [])
-	const getAnimeData = () => {
-		fetch('https://api.jikan.moe/v3/search/anime?q=Naruto&page=1')
-		.then(response => response.json())
-		.then(data => {
-			console.log(data)
-		})
-		.catch(err => {
-			console.log(err)
-		})
-	}
 			
 
   return (
