3b6528478141e39e846f38555183e3ab49ac9a1a
spazevic <spazevic@yahoo.com>
2020-06-11 16:38:47 -0700

    merge time

--- src/content/pages/AnimeInfo.js
+++ src/content/pages/AnimeInfo.js
@@ -52,8 +52,6 @@ const AnimeInfo = props => {
       		{animeData.synopsis}
       		{genreList}
       	</div>
-      {props.animeId}
-      {animeData.title}
 	  <div>
 	  <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/241059426&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
       <div style={{fontSize: "10px", color: "#cccccc",lineBreak: "anywhere", wordBreak: "normal", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif", fontWeight: "100"}}>
