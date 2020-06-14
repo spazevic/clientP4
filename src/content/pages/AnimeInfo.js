b823cd8be409b25a99d145d2a696c45c3d7acce4
spazevic <spazevic@yahoo.com>
2020-06-13 20:05:49 -0700

    hehe

--- src/content/pages/AnimeInfo.js
+++ src/content/pages/AnimeInfo.js
@@ -3,12 +3,17 @@ const AnimeInfo = props => {
 	let [animeData, setAnimeData] = useState({})
 	let [newComment, setNewComment] = useState('')
 	let [comments, setComments] = useState({})
+	let [currentRating, setCurrentRating] = useState()
 	let [userList, setUserList] = useState([])
 	let [userRating, setUserRating] = useState()
 	useEffect(() => {
 		getAnimeData()
+		if (!comments.length) {
+			addCommentData()
+		}
 		getComments()
 		findUsers()
+		getFaves()
 	}, [])
 	const getAnimeData = () => {
 		fetch('https://api.jikan.moe/v3/anime/' + props.animeId)
@@ -22,7 +27,6 @@ const AnimeInfo = props => {
 			console.log(err)
 		})
 	}
-	console.log(props.animeId)
 
 	const addCommentData = () => {
   		fetch(process.env.REACT_APP_SERVER_URL + 'comment', {
@@ -44,9 +48,6 @@ const AnimeInfo = props => {
 
 	const addNewComment = e => {
 		e.preventDefault()
-		if (!comments.length) {
-			addCommentData()
-		}
   		fetch(process.env.REACT_APP_SERVER_URL + 'comment', {
 	      method: 'PUT',
 	      body: JSON.stringify({
@@ -65,7 +66,7 @@ const AnimeInfo = props => {
 	    	console.log(err)
 		})
 	}
-
+	//this loads the comments for the specific page
 	const getComments = () => {
 		fetch(process.env.REACT_APP_SERVER_URL + 'comment/' + props.animeId, {
 	      method: 'GET',
@@ -87,26 +88,28 @@ const AnimeInfo = props => {
 		    	console.log(err)
 		})
 	}
-
-	const addFavesData = () => {
-  		fetch(process.env.REACT_APP_SERVER_URL + 'favorites', {
-	      method: 'POST',
-	      body: JSON.stringify({
-	      	user: props.user._id
-	      }),
-	      headers: {
-	        'Content-Type': 'application/json'
-	      }
-	    })
-	    .then(response => {
-	    	console.log(response)
-	    })
-	    .catch(err => {
-	    	console.log(err)
-		})
-	}
-
-	const addNewFaves = () => {
+	//this sets the initial favorites db for the user, 
+	//only accessed if they havent favorited anything
+	// const addFavesData = () => {
+ //  		fetch(process.env.REACT_APP_SERVER_URL + 'favorites', {
+	//       method: 'POST',
+	//       body: JSON.stringify({
+	//       	user: props.user._id
+	//       }),
+	//       headers: {
+	//         'Content-Type': 'application/json'
+	//       }
+	//     })
+	//     .then(response => {
+	//     	console.log(response)
+	//     })
+	//     .catch(err => {
+	//     	console.log(err)
+	// 	})
+	// }
+	//this adds to the users anime favorites
+	const addNewFaves = e => {
+		e.preventDefault()
   		fetch(process.env.REACT_APP_SERVER_URL + 'favorites', {
 	      method: 'Put',
 	      body: JSON.stringify({
@@ -129,6 +132,34 @@ const AnimeInfo = props => {
 		})
 	}
 
+	const getFaves = () => {
+		fetch(process.env.REACT_APP_SERVER_URL + 'favorites/' + props.user._id, {
+	      method: 'GET',
+	      headers: {
+	        'Content-Type': 'application/json'
+	      }
+	    })
+	   	.then(response => response.json()
+		    .then(response => {
+		    	console.log(response)
+		    	for (let i = 0; i <response.faves.length; i++) {
+		    		console.log(response.faves[i])
+		    		if (response.faves[i].animeId == props.animeId) {
+		    		setCurrentRating(response.faves[i].rating)
+		    	}
+		    	}
+
+		    })
+		    .catch(err => {
+		    	console.log(err)
+			})
+		)
+		.catch(err => {
+		    	console.log(err)
+		})
+	}
+
+	//this loads the user list, this is to assign the user for each comment that was posted
 	const findUsers = () => {
 		fetch(process.env.REACT_APP_SERVER_URL + 'auth/users', {
 	      method: 'GET',
@@ -152,15 +183,12 @@ const AnimeInfo = props => {
 	console.log(userList)
 
 
-
-
+	//this loads all the genres for the displayed anime
 	let genreList;
-	//check for genres so it doesn't try to access the array before it exists
 	if (animeData.genres) {
 		console.log('hi')
 		console.log(animeData.genres[0])
 		genreList = animeData.genres.map((g, i) => {
-			console.log(g.name)
 			return (
 				<div key = {i}>
 				{g.name}
@@ -170,13 +198,11 @@ const AnimeInfo = props => {
 	}
 
 	console.log(comments)
+	//loads the comments for displayed anime
 	let commentList;
-
 	if (comments.animeId) {
-
 		console.log('hey bro i got comments')
 		commentList = comments.userComment.map((c, i) => {
-			console.log('yoyoyo')
 			console.log(props.user)
 			console.log(c.user)
 			let name
@@ -195,6 +221,24 @@ const AnimeInfo = props => {
 		})
 	}
 
+	let rating;
+	console.log(currentRating)
+	if(currentRating) {
+		rating = (
+			<div>kek</div>
+		)
+
+	} else {
+		rating = (
+		<form onSubmit={addNewFaves}>
+      		<input type='number' min='1' max='10' onChange={e => 
+      		setUserRating(e.target.value)}/>
+      		<input type='submit' value ='Give a rating' />
+      	</form>
+		)
+	}
+	
+
 
 
   return (
@@ -209,9 +253,11 @@ const AnimeInfo = props => {
       		setNewComment(e.target.value)}/>
     		<input type='submit' value='Add New Comment' />
     	</form>
+    	{rating}
     	<div>
     		{animeData.score}
       	</div>
+      	{currentRating}
       	<div>
       		<img src={animeData.image_url} />
       	</div>
@@ -220,14 +266,7 @@ const AnimeInfo = props => {
       		{genreList}
       		{commentList}
       	</div>
-      	<form onSubmit={addFavesData}>
-      		<input type='submit' value='create rating' />
-      	</form>
-      	<form onSubmit={addNewFaves}>
-      		<input type='number' min='1' max='10' onChange={e => 
-      		setUserRating(e.target.value)}/>
-      		<input type='submit' value ='Give a rating' />
-      	</form>
+
 	  <div>
 	  <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/594376845&color=%23b98eef&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
       <div style={{fontSize: "10px", color: "#cccccc",lineBreak: "anywhere", wordBreak: "normal", overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", fontFamily: "Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif", fontWeight: "100"}}>
